92d1980d5ab544612a8f6a8af2de9884a56ba55b
vsnaMary <mary-berry36@yandex.ru>
2017-10-01 02:27:25 +0300

    очернение картинки

--- js/on-click-handlers.js
+++ js/on-click-handlers.js
@@ -1,5 +1,26 @@
 let currentImage = null;
 
+function toBlackAndWhite(image) {
+    for (let i = 0; i < image.height; i++) {
+        for (let j = 0; j < image.width; j++) {
+            let idx = (4 * i) * image.width + (4 * j);
+
+            let red = image.data[idx];
+            let green = image.data[idx + 1];
+            let blue = image.data[idx + 2];
+            let alpha = image.data[idx + 3];
+            let average = (red + green + blue) / 3;
+
+            image.data[idx] = average;
+            image.data[idx + 1] = average;
+            image.data[idx + 2] = average;
+            image.data[idx + 3] = alpha;
+        }
+    }
+
+    return image;
+}
+
 function repaint() {
     console.log(currentImage);
 
@@ -14,7 +35,13 @@ function repaint() {
     canvas.height = canvas.width * coof;
 
     let ctx = canvas.getContext("2d");
+
     ctx.drawImage(currentImage, 0, 0, canvas.width, canvas.height);
+
+    if (document.getElementById("image-bw").value === "1") {
+        let fromCanvas = ctx.getImageData(0, 0, canvas.width, canvas.height);
+        ctx.putImageData(toBlackAndWhite(fromCanvas), 0, 0);
+    }
 }
 
 function onFilesSelected(e) {
@@ -39,3 +66,7 @@ fileInput.onchange = onFilesSelected;
 let imageWidth = document.getElementById("image-width");
 imageWidth.oninput = repaint;
 
+document.getElementById("image-bw").addEventListener("change", function () {
+    this.value ^= 1;
+    repaint();
+});
